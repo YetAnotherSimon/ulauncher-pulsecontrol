6ecc53c2eb141953a3375429fa120181ce019c79
CeroProgramming <programming@quantcero.org>
2020-09-15 21:30:05 +0200

    Added developement logging

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
+from logging import Logger, getLogger, FileHandler, StreamHandler, Formatter, DEBUG
+
 from ulauncher.api.client.Extension import Extension
 from ulauncher.api.client.EventListener import EventListener
-from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
+from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent, SystemExitEvent
 
 from pulsecontrol.pulse import set_volume, set_device
 from pulsecontrol.actions import get_volume_actions, get_device_actions
+
+
+def setup_logger() -> Logger:
+    logger = getLogger('ulauncher-pulsecontrol')
+    logger.setLevel(DEBUG)
+    formatter = Formatter('%(asctime)s : %(levelname)s : %(name)s : %(message)s')
+    file_handler = FileHandler(filename='/tmp/ulauncher-pulsecontrol.log', encoding='utf-8', mode='w')
+    file_handler.setFormatter(formatter)
+    stream_handler = StreamHandler()
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    logger.addHandler(stream_handler)
+    return logger
+
+
+def dismantle_logger(logger: Logger):
+    handlers = logger.handlers.copy()
+    for handler in handlers:
+        handler.close()
+        logger.removeHandler(handler)
 
 
 class PulseControl(Extension):
@@ -34,4 +56,8 @@ class KeywordQueryEventListener(EventListener):
 
 
 if __name__ == '__main__':
-    PulseControl().run()
+    LOGGER = setup_logger()
+    try:
+        PulseControl().run()
+    except SystemExitEvent:
+        dismantle_logger(LOGGER)
